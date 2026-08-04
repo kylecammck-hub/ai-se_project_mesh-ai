580065619504818989dfa35a055b4832ead1a5f1
kylecammck-hub <kylecammck@gmail.com>
2026-08-04 12:40:43 -0500

    Add authentication middleware to query router

--- src/routes/query.ts
+++ src/routes/query.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import auth from '../middleware/auth.js';
 import { postQuery } from '../controllers/query.js';
 
 const queryRouter = Router();
+
+queryRouter.use(auth);
 
 queryRouter.post('/', postQuery);
 
