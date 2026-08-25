6c579b4cb92984424a9a17f184ba274c6281fb5f
kylecammck-hub <kylecammck@gmail.com>
2026-08-25 12:44:53 -0500

    Wire up React Router in App.tsx

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,21 @@
-import "./App.css"
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import AppLayout from '../AppLayout/AppLayout';
+import Intro from '../../pages/Intro/Intro';
+import Chat from '../../pages/Chat/Chat';
+import KnowledgeBase from '../../pages/KnowledgeBase/KnowledgeBase';
 
 function App() {
   return (
     <div className="app">
-      App
+      <Routes>
+        <Route path="/" element={<Intro />} />
+
+        <Route element={<AppLayout />}>
+          <Route path="/knowledge" element={<KnowledgeBase />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
